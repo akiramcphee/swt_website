a2caf9355ce5d816bc9635b227473fc547aab815
akiramcphee <85497059+akiramcphee@users.noreply.github.com>
2021-11-06 20:02:31 +1100

    feat: Log page looking clean

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 //import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './Home/Home';
-import About from './About/About';
+// import About from './About/About';
 import Projects from './Our_projects/Projects';
-import Log from './Log/Log';
-import Profiles from './Profiles/Profiles';
-import LogPost from './components/LogPost';
+// import Log from './Log/Log';
+// import Profiles from './Profiles/Profiles';
+
 
 function App() {
   return(
     <div>
-      <Log></Log>
-      {/* <Log></Log> */}
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Home}/>
+        </Routes>
+      </Router>
+      <Projects></Projects>
     </div>
   );
 }
