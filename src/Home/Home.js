3bd14f9fe62e16db9ea780aff4273fc85d2a4f35
akiramcphee <85497059+akiramcphee@users.noreply.github.com>
2021-11-06 20:00:18 +1100

    feat: Log page looking clean

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+// import Navbar from '../components/Navbar'
+// import Heading from '../components/Heading/Heading';
+import Homepage from '../components/Homepage_Title/Homepage'
 import './Home.css'
 import team from '../images/team.jpg'
 
@@ -8,17 +10,19 @@ console.log(team);
 export default function Home() {
     return (
         <div>
-            <div class="header">
-                <Navbar></Navbar>
-            </div>
             <div className="row">
-                <div className="col-12">
-                    <div className = "background_image">
-                        <img src={team} alt="SWT"></img>
-                    </div>
+
+            <div className="col-12">
+                <div className = "background_image">
+                    <img src={team} alt="SWT"></img>
                 </div>
             </div>
-
+            
+            </div>
+            
+            <div>
+                <Homepage></Homepage>
+            </div>
             
         </div>
     )
