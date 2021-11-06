034bfa9efa5010affbaa32ef7a0cf678e51434d3
Taylor Chan <taylorchan.0119@gmail.com>
2021-11-06 17:08:33 +1100

    fix: resolve compilation errors

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Navbar from './components/Navbar';
 import Home from './Home/Home';
 // import About from './About/About';
 import Projects from './Our_projects/Projects';
-import Log from './Log/Log';
-import Profiles from './Profiles/Profiles';
+// import Log from './Log/Log';
+// import Profiles from './Profiles/Profiles';
 
 
 function App() {
--- src/Our_projects/Projects.js
+++ src/Our_projects/Projects.js
@@ -4,9 +4,6 @@ import './Projects.css'
 export default function Projects() {
     return (
         <div>
-            <div className="header">
-                <Navbar></Navbar>
-            </div>
             <div className = "row">
                 <div className = "col-4">
                     <h1>Projects</h1>
