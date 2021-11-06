b9852d935f0796991b888127225b097d1ba41333
akiramcphee <85497059+akiramcphee@users.noreply.github.com>
2021-11-06 21:15:00 +1100

    feat: routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './Home/Home';
-// import About from './About/About';
+import About from './About/About';
 import Projects from './Our_projects/Projects';
 import Log from './Log/Log';
 import Profiles from './Profiles/Profiles';
@@ -15,10 +15,13 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact component={Home}/>
+          <Route path='/' exact component={Home} element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/projects' exact component={Projects} element={<Projects/>}/>
+          <Route path='/log' exact component={Log} element={< Log/>}/>
+          <Route path='/profiles' exact component={Profiles} element={<Profiles />}/>
         </Routes>
       </Router>
-      <Profiles></Profiles>
     </div>
   );
 }
