716d7bb2ebe6aaf7dc285db94329d05090f5049c
akiramcphee <85497059+akiramcphee@users.noreply.github.com>
2021-11-06 13:36:09 +1100

    feat: basic about layout

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './Home/Home';
+import About from './About/About';
+
 
 function App() {
   return(
     <div>
-        <Home></Home>
+        {/* <Home></Home> */}
+        <About></About>
     </div>
   );
 }
