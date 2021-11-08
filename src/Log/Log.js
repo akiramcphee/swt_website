ea8f893073df94cbf9dcc789c5dba2b20d16463d
akiramcphee <85497059+akiramcphee@users.noreply.github.com>
2021-11-08 18:13:18 +1100

    feat: fat fat json

--- src/Log/Log.js
+++ src/Log/Log.js
@@ -5,30 +5,32 @@ import './Log.css'
 
 console.log(stuff);
 
-export default function Log() {
+export default function Log(props) {
+	const projectName = props.project;
+	let log = require('../data.json');
 	return (
 		<div className = "back">
 			<div className="row"></div>
 			<div className="row">
-				<div class="project">Project</div>
+				<div class="project">{log['project-name'][projectName]['full-name']}</div>
 			</div>
 			<div className="row">
 				<div class="container">
 					<div className = "col-6">
-							<LogPost week = "week_4"/>
+							<LogPost project= {projectName} week = "week_4"/>
 					</div>
 					<div className = "col-6">
-							<LogPost week = "week_3"/>
+							<LogPost project= {projectName} week = "week_3"/>
 					</div>
 					</div>
 				</div>
 				<div className="row">
 				<div class="container">
 					<div className = "col-6">
-							<LogPost week = "week_2"/>
+							<LogPost project= {projectName} week = "week_2"/>
 					</div>
 					<div className = "col-6">
-							<LogPost week = "week_1"/>
+							<LogPost project= {projectName} week = "week_1"/>
 					</div>
 					</div>
 				</div>
--- src/components/LogPost/LogPost.js
+++ src/components/LogPost/LogPost.js
@@ -4,13 +4,13 @@ import './LogPost.css'
 
 
 export default function LogPost(props) {
-    let log = require('./LogPost.json');
-    const photo = require(`../../images/${log[props.week].image}`).default;
+    let log = require('../../data.json');
+    const photo = require(`../../images/${log['project-name'][props.project][props.week].image}`).default;
     return (
         <div>
             <div className = "background-border">
-                <h3 className = "logPost_date">{log[props.week].date}</h3>
-                <p className = "logPost_description">{log[props.week].overview}</p>
+                <h3 className = "logPost_date">{log['project-name'][props.project][props.week].date}</h3>
+                <p className = "logPost_description">{log['project-name'][props.project][props.week].overview}</p>
                 <div className = "log-image">
                     <img src={photo} alt="SWT"></img> 
                 </div>
