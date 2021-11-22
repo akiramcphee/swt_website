97c478b2aa26b76954241b6aeb554c7b34302bfa
Taylor Chan <taylorchan.0119@gmail.com>
2021-11-22 16:26:10 +1100

    chore: update all log text

--- src/Our_projects/Projects.js
+++ src/Our_projects/Projects.js
@@ -31,6 +31,14 @@ export default function Projects() {
             <div className="row">
                 <div className="col-12">
                     <div className = "read-more">
+                        <i class="fas fa-chevron-up"></i>
+                        <h2>Click on any of the images above to see our progress log</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <div className = "read-more">
                         <h2>Learn about our sensors</h2>
                         <i class="fas fa-chevron-down"></i>
                     </div>
--- src/components/LogPost/LogPost.js
+++ src/components/LogPost/LogPost.js
@@ -3,12 +3,12 @@ import './LogPost.css'
 
 export default function LogPost(props) {
     let log = require('../../data.json');
-    const photo = require(`../../images/${log['project-name'][props.project][props.week].image}`).default;
+    const photo = require(`../../images/${log['project-name'][props.project][props.post].image}`).default;
     return (
         <div>
             <div className = "background-border">
-                <h3 className = "logPost_date">{log['project-name'][props.project][props.week].date}</h3>
-                <p className = "logPost_description">{log['project-name'][props.project][props.week].overview}</p>
+                <h3 className = "logPost_date">{log['project-name'][props.project][props.post].date}</h3>
+                <p className = "logPost_description">{log['project-name'][props.project][props.post].overview}</p>
                 <div className = "log-image">
                     <img className = "log-image-size" src={photo} alt="SWT"></img> 
                 </div>
