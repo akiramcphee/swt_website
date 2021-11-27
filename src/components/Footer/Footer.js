85deb2c5993fb7feb0db01584cb33c800143dd08
Taylor Chan <taylorchan.0119@gmail.com>
2021-11-27 16:06:27 +1100

    feat: adaptive footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,12 +4,12 @@ import './Footer.css'
 export default function Footer() {
 	return (
 		<div className = "footer">
-			<div className="col-6">
+			<div className="col-7">
 				<div className="swt-footer-text">
 					<p>Smart Wearable Tech</p>
 				</div>  
 			</div>
-			<div className="col-6">
+			<div className="col-5">
 				<div className="footer-logo">
 				<img 
 					className='swt-logo-footer' 
