683d9ad820c1e0df381f4a99f09b1260ae35fdfb
akiramcphee <85497059+akiramcphee@users.noreply.github.com>
2021-11-28 02:26:15 +1100

    feat: footer redesign

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,20 +4,24 @@ import './Footer.css'
 export default function Footer() {
 	return (
 		<div className = "footer">
-			<div className="col-7">
-				<div className="swt-footer-text">
-					<p>Smart Wearable Tech</p>
-				</div>  
-			</div>
-			<div className="col-5">
-				<div className="footer-logo">
-				<img 
-					className='swt-logo-footer' 
-					src='../../logo_white.svg'
-					alt='Smart Wearable Tech logo'
-				/>
-				</div> 
-			</div>			
+			<div className = 'row'>
+				<div className="col-12">
+					<div className="footer-logo">
+					<img 
+						className='swt-logo-footer' 
+						src='../../logo_white.svg'
+						alt='Smart Wearable Tech logo'
+					/>
+					</div> 
+				</div>	
+			</div>	
+			<div className = 'row'>
+				<div className="col-12">
+					<div className="swt-footer-text">
+						<p>Smart Wearable Tech</p>
+					</div>  
+				</div>
+			</div>	
 		</div>
 	)
 }
