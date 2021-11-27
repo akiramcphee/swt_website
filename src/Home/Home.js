3986f4f5a947ac8687d5aa4c72bd536b03c3ce98
akiramcphee <85497059+akiramcphee@users.noreply.github.com>
2021-11-27 14:56:45 +1100

    feat: project cards in home page now

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,6 +5,7 @@ import HomeText from '../components/Homepage_Title/HomeText'
 import './Home.css'
 import team from '../images/team.jpg'
 import { Button } from '../components/Button/Button';
+import Project_cards from '../components/Project_cards/Project_cards'
 
 console.log(team);
 
@@ -19,6 +20,34 @@ export default function Home() {
                 </div>
                 <div className="row">
                     <div class="home-swt-button"><Button className = "pitch-button" buttonStyle='btn--outline--large' page='pitch'>VIEW THE PITCH</Button></div>
+                </div>
+                
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <div className = "read-more">
+                        <i class="fas fa-chevron-up"></i>
+                        <h2>Our Projects</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4">
+                    <Project_cards projectName='speech'/>  
+                </div>
+                <div className="col-4">
+                    <Project_cards projectName='glove'/>
+                </div>
+                <div className="col-4">
+                    <Project_cards projectName='pulse'/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12">
+                    <div className = "read-more">
+                        <i class="fas fa-chevron-up"></i>
+                        <h2>Click on any of the images above to see our progress log</h2>
+                    </div>
                 </div>
             </div>
         </div>
